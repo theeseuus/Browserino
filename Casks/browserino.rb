3292268297b4571372bb242448acd95913093b83
troy <67619667+theeseuus@users.noreply.github.com>
2026-08-24 16:29:10 +1200

    Use fork releases for cask and add release workflow

--- Casks/browserino.rb
+++ Casks/browserino.rb
@@ -2,7 +2,7 @@ cask "browserino" do
   version "1.1.16"
   sha256 "8de2b7fbd8ad0eb2510de413d9f08039fbdf69d208f5aa9416f91ef21509a5ba"
 
-  url "https://raw.githubusercontent.com/theeseuus/Browserino/main/artifacts/Browserino-#{version}.zip"
+  url "https://github.com/theeseuus/Browserino/releases/download/v#{version}/Browserino.zip"
   name "Browserino"
   desc "Browserino is a tiny browser selector for MacOS written in SwiftUI"
   homepage "https://github.com/theeseuus/Browserino"
