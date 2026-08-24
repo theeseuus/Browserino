20e3a08c011acd07e24e67508aafd162364f22a1
troy <67619667+theeseuus@users.noreply.github.com>
2026-08-24 16:21:10 +1200

    Use upstream release asset URL for browserino cask

--- Casks/browserino.rb
+++ Casks/browserino.rb
@@ -2,10 +2,10 @@ cask "browserino" do
   version "1.1.16"
   sha256 "8de2b7fbd8ad0eb2510de413d9f08039fbdf69d208f5aa9416f91ef21509a5ba"
 
-  url "https://github.com/theeseuus/Browserino/releases/download/v#{version}/Browserino.zip"
+  url "https://github.com/AlexStrNik/Browserino/releases/download/v#{version}/Browserino.zip"
   name "Browserino"
   desc "Browserino is a tiny browser selector for MacOS written in SwiftUI"
-  homepage "https://github.com/theeseuus/Browserino"
+  homepage "https://github.com/AlexStrNik/Browserino"
 
   auto_updates false
   depends_on macos: :ventura
