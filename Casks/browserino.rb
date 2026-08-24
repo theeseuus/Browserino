1e82fe4ea33ffa6e30d2e4848e22bc1fa64b63d5
troy <67619667+theeseuus@users.noreply.github.com>
2026-08-24 16:54:12 +1200

    Use stable release artifact filename and publish legacy zip name

--- Casks/browserino.rb
+++ Casks/browserino.rb
@@ -2,7 +2,7 @@ cask "browserino" do
   version "1.1.17"
   sha256 "8de2b7fbd8ad0eb2510de413d9f08039fbdf69d208f5aa9416f91ef21509a5ba"
 
-  url "https://github.com/theeseuus/Browserino/releases/download/v#{version}/Browserino-#{version}.zip"
+  url "https://github.com/theeseuus/Browserino/releases/download/v#{version}/Browserino.zip"
   name "Browserino"
   desc "Browserino is a tiny browser selector for MacOS written in SwiftUI"
   homepage "https://github.com/theeseuus/Browserino"
